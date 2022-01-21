4cee1651d03206c868f030555093966ff1449187
jribeiroo <nicolas.jnr10@outlook.com>
2022-01-21 02:01:37 -0300

    cria funções de escola de nivel estrela

--- StarRate.js
+++ StarRate.js
@@ -2,40 +2,72 @@
 class StarRate extends HTMLElement {
   constructor() {
     super();
-    this.build()
+    this.build();
   }
 
   //responsavel pela junção de todos os elementos
   build() {
-    const shadow = this.attachShadow({ mode: "open" });
+    const shadow = this.attachShadow({ mode: 'open' });
 
     //    adicona filhos
     shadow.appendChild(this.style());
-    const rater = this.createRater()
-    const stars = this.createStars()
+    const rater = this.createRater();
+    this.stars = this.createStars();
+    //coloca os span dentro da div
+    this.stars.forEach(star => rater.appendChild(star));
 
-    //coloca os span dentro da div 
-    stars.forEach(star => rater.appendChild(star))
+    this.resetRating();
 
-
-    shadow.appendChild(rater)
+    shadow.appendChild(rater);
   }
 
-  createRater(){
-      const rater = document.createElement('div')
-      rater.classList.add('star-rate')
-      return rater
+  createRater() {
+    const rater = document.createElement("div");
+    rater.classList.add("star-rater");
+    rater.addEventListener("mouseout", this.resetRating.bind(this));
+    return rater;
   }
 
-  createStars(){
-      const createStar = (_,id) => {
-          const star = document.createElement('span')
-          star.classList.add('star')
-          star.setAttribute('data-value', Number(id) + 1)
-          star.innerHTML = '&#9733;'
-          return star
-      }
-      return Array.from({length:5}, createStar)
+  createStars() {
+    const createStar = (_, id) => {
+      const star = document.createElement("span");
+      star.classList.add("star");
+      star.setAttribute("data-value", Number(id) + 1);
+      star.innerHTML = "&#9733;";
+
+      //bind é a junção dos 2 "this" o da classe e o da função criada
+      star.addEventListener("click", this.setRating.bind(this));
+      star.addEventListener("mouseover", this.ratingHover.bind(this));
+      return star;
+    };
+    return Array.from({ length: 5 }, createStar);
+  }
+
+  resetRating() {
+    this.currentRatingValue = this.getAttribute("data-rating") || 0;
+    this.hightLightRating();
+  }
+
+  setRating(event) {
+    this.setAttribute(
+      "data-rating",
+      event.currentTarget.getAttribute("data-value")
+    );
+  }
+
+  ratingHover(event) {
+    this.currentRatingValue = event.currentTarget.getAttribute("data-value");
+    this.hightLightRating();
+    console.log(this);
+  }
+
+  hightLightRating() {
+    this.stars.forEach((star) => {
+      star.style.color =
+        this.currentRatingValue >= star.getAttribute("data-value")
+          ? "yellow"
+          : "gray";
+    });
   }
 
   style() {
@@ -43,10 +75,9 @@ class StarRate extends HTMLElement {
     style.textContent = `
     .star{
         font-size:5rem;
-        color:grey;
         cursor:pointer;
-    }
-    `
+        color: gray
+    }`;
     return style;
   }
 }
@@ -54,4 +85,4 @@ class StarRate extends HTMLElement {
 // recebe 2 argumentos, 1º como string
 // que vai dar o nome ao elemento que vai ser criado para uso no html
 // 2º como nome da classe que foi criada
-customElements.define("star-rate", StarRate);
+customElements.define("star-rater", StarRate);
